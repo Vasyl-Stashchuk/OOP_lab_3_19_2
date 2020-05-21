052a38adfd6965529a2e1f0676f1ca130fdec0ba
Роман Данилевич <dwordchanel@gmail.com>
2020-05-21 14:36:08 +0300

    Add project files.

---
+++ OOP_lab_3_19_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_19_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("text.txt");
+
+            string str = file.ReadToEnd();
+
+            string[] sentences = str.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+            str = "";
+
+            for (int i = 0; i < sentences.Length; ++i)
+            {
+                str += sentences[i] + " ";
+            }
+
+            sentences = str.Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Кiлькiсть речень: {0}", sentences.Length);
+        }
+    }
+}
